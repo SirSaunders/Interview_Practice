66ebb0b1c28c7e914d76b56764f96682d476ec4a
Johnathan Saunders <Saundersokj@gmail.com>
2018-05-08 18:04:01 -0400

    added fib_it tests

--- General/Fibbonaci.py
+++ General/Fibbonaci.py
@@ -1,13 +1,13 @@
 ###Write fibbonaci iteratively and recursively (bonus: use dynamic programming)###
 
-def fibbonaci_iteratively(index):
+def fibonaccii_iteratively(index):
     a = 1
-    b = 1 #starting numbers
+    b = 1  # starting numbers
     c = None
-    if( index < 0 ):
-        return None # must give a number greater than 0 as index
+    if (index < 0):
+        return None  # must give a number greater than 0 as index
     # if less than index 2 then it must be 1
-    if(index < 2):
+    if (index < 2):
         return a
     for i in range(1, index):
         c = a + b
@@ -15,4 +15,11 @@ def fibbonaci_iteratively(index):
         b = c
     return c
 
-print(fibbonaci_iteratively(0))
+
+def test_fibonacci_iteratively():
+    assert fibonaccii_iteratively(0) == 1
+    assert fibonaccii_iteratively(1) == 1
+    assert fibonaccii_iteratively(2) == 2
+    assert fibonaccii_iteratively(3) == 3
+    assert fibonaccii_iteratively(4) == 5
+    assert fibonaccii_iteratively(5) == 8
