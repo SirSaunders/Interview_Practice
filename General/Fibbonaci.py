783d97d4092300cb041c25d4b05b3ba2090ab1a0
Johnathan Saunders <Saundersokj@gmail.com>
2018-05-08 18:24:48 -0400

    fixed a starting at 1 for fib_it and add fib_recursively

--- General/Fibbonaci.py
+++ General/Fibbonaci.py
@@ -1,14 +1,14 @@
 ###Write fibbonaci iteratively and recursively (bonus: use dynamic programming)###
 
 def fibonaccii_iteratively(index):
-    a = 1
+    a = 0
     b = 1  # starting numbers
     c = None
-    if (index < 0):
-        return None  # must give a number greater than 0 as index
+    if (index <= 0):
+        return a  # must give a number greater than 0 as index
     # if less than index 2 then it must be 1
     if (index < 2):
-        return a
+        return b
     for i in range(1, index):
         c = a + b
         a = b
@@ -17,9 +17,26 @@ def fibonaccii_iteratively(index):
 
 
 def test_fibonacci_iteratively():
-    assert fibonaccii_iteratively(0) == 1
+    assert fibonaccii_iteratively(0) == 0
     assert fibonaccii_iteratively(1) == 1
-    assert fibonaccii_iteratively(2) == 2
-    assert fibonaccii_iteratively(3) == 3
-    assert fibonaccii_iteratively(4) == 5
-    assert fibonaccii_iteratively(5) == 8
+    assert fibonaccii_iteratively(2) == 1
+    assert fibonaccii_iteratively(3) == 2
+    assert fibonaccii_iteratively(4) == 3
+    assert fibonaccii_iteratively(5) == 5
+
+
+def fibonacci_recursively(index):
+    if index <= 0:
+        return 0
+    if index < 2:
+        return 1
+    return fibonacci_recursively(index-2) + fibonacci_recursively(index-1)
+
+
+def test_fibonacci_recursively():
+    assert fibonacci_recursively(0) == 0
+    assert fibonacci_recursively(1) == 1
+    assert fibonacci_recursively(2) == 1
+    assert fibonacci_recursively(3) == 2
+    assert fibonacci_recursively(4) == 3
+    assert fibonacci_recursively(5) == 5
