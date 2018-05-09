c963421761ec01c95297e0bab960f8bfabc41575
Johnathan Saunders <Saundersokj@gmail.com>
2018-05-08 20:30:41 -0400

    reverse string completed

---
+++ Strings/reverse_string.py
@@ -0,0 +1,24 @@
+# Reverse a String iteratively and recursively
+
+def reverse_iteratively(string):
+    new_string = str()
+    for char in reversed(string):
+        new_string += char
+    return new_string
+
+
+def test_reverse_iteratively():
+    assert reverse_iteratively('whats up') == 'pu stahw'
+
+#thought this was a cool one liner for python
+def reverse_recursively(string):
+    print('')
+    length = len(string)
+    if length == 1:
+        return string
+    return string[length-1] + reverse_recursively(string[:-1])
+
+def test_reverse_recursively():
+    assert reverse_recursively('whats up') == 'pu stahw'
+
+
