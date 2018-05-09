ce820b46cb1b1c0b7cfded81e83d359a1467e2cb
Johnathan Saunders <Saundersokj@gmail.com>
2018-05-09 10:21:15 -0400

    added inserstion sort

---
+++ Sorting/insertion_sort.py
@@ -0,0 +1,22 @@
+# Implement insertion sort
+
+def insertion_sort(array):
+    for i in range(len(array)):
+        j = i
+        while j > 0 and array[j - 1] > array[j]:
+            # swap array[j-1]> array[j]
+            array = swap(array, j - 1, j)
+            j -= 1
+    return array
+
+
+def swap(array, a, b):
+    swapA = array[a]
+    swapB = array[b]
+    array[a] = swapB
+    array[b] = swapA
+    return array
+
+
+def test_insertion_sort():
+    assert (insertion_sort([3, 2, 4, 1]) == [1, 2, 3, 4])
